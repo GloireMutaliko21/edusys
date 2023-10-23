81d9a92435c1f477f5b5bb6751114a380dfcf20a
Gloire Mutaliko <mufunyig@gmail.com>
2023-10-23 11:33:30 +0200

    feat: add toats

--- src/components/modals/index.toast.tsx
+++ src/components/modals/index.toast.tsx
@@ -3,6 +3,7 @@ import { message as Toast } from 'antd';
 import { useEffect } from 'react';
 import { useAppDispatch } from '@/hooks/useAppDispatch';
 import { useAppSelector } from '@/hooks/useAppSelector';
+import { setAuthIsError, setAuthIsSuccess } from '@/redux/auth/auth.slice';
 
 const ToastMessageManager = () => {
 	const [toast, contextHolder] = Toast.useMessage({ maxCount: 1 });
@@ -11,20 +12,20 @@ const ToastMessageManager = () => {
 	/**
 	 * Auth toast messages
 	 */
-	// const { status: authStatus, message: authMessage } = useAppSelector(
-	// 	(state) => state.auth
-	// );
+	const { status: authStatus, message: authMessage } = useAppSelector(
+		(state) => state.auth
+	);
 
-	// useEffect(() => {
-	// 	if (authStatus.isSuccess) {
-	// 		authMessage && toast.success(authMessage);
-	// 		dispach(setAuthIsSuccess(false));
-	// 	}
-	// 	if (authStatus.isError) {
-	// 		authMessage && toast.error(authMessage);
-	// 		dispach(setAuthIsError(false));
-	// 	}
-	// }, [authStatus]);
+	useEffect(() => {
+		if (authStatus.isSuccess) {
+			authMessage && toast.success(authMessage);
+			dispach(setAuthIsSuccess(false));
+		}
+		if (authStatus.isError) {
+			authMessage && toast.error(authMessage);
+			dispach(setAuthIsError(false));
+		}
+	}, [authStatus]);
 
 	return (
 		<>
