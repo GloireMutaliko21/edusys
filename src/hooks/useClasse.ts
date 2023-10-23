d1c2cd06f7525e959da4e9be8eb0177ceafe4724
Gloire Mutaliko <mufunyig@gmail.com>
2023-10-23 19:35:49 +0200

    feat: create classe hook

--- src/hooks/useClasse.ts
+++ src/hooks/useClasse.ts
@@ -2,26 +2,26 @@ import { useEffect } from 'react';
 import { useAppDispatch } from './useAppDispatch';
 import { useAppSelector } from './useAppSelector';
 import useAuth from './useAuth';
-import { getStaffs, setStaffIsError } from '@/redux/staff/staff.slice';
+import { getClasses, setClasseIsError } from '@/redux/classes/class.slice';
 
-const useStaff = () => {
-	const { staffs, selectedStaff, message, status } = useAppSelector(
-		(state) => state.staff
+const useClasses = () => {
+	const { classes, selectedClasse, message, status } = useAppSelector(
+		(state) => state.class
 	);
 	const dispatch = useAppDispatch();
 	const { isLogin } = useAuth();
 
 	useEffect(() => {
 		if (isLogin) {
-			dispatch(getStaffs());
-		} else dispatch(setStaffIsError(true));
+			dispatch(getClasses());
+		} else dispatch(setClasseIsError(true));
 	}, [dispatch, isLogin, message]);
 	return {
-		staffs,
-		selectedStaff,
+		classes,
+		selectedClasse,
 		status,
 		message,
 	};
 };
 
-export default useStaff;
+export default useClasses;
